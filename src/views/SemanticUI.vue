3f009dccfedba67de075a42dc805892e7f0ac722
unknown <rajovicmirko@gmail.com>
2020-07-21 09:02:35 +0200

    Small fix SemanticUI page

--- src/views/SemanticUI.vue
+++ src/views/SemanticUI.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="semanticui">
-    <div
-      style="width: 100%; display: flex; justify-content: space-evenly; align-items: center;"
-    >
+    <div style="width: 100%; display: flex; justify-content: space-evenly; align-items: center;">
       <a
         class="git-link"
         target="_blank"
         href="https://github.com/RajovicMirko/vue-form-terminator-live/blob/master/src/views/SemanticUI.vue"
-        >Git page code sample</a
-      >
+      >Git page code sample</a>
 
       <div>
-        <label for="errorMessagePosition" style="margin-right: 0.5rem"
-          >Error message position</label
-        >
+        <label for="errorMessagePosition" style="margin-right: 0.5rem">Error message position</label>
         <select
           name="errorMessagePosition"
           id="errorMessagePosition"
@@ -27,16 +22,8 @@
     </div>
 
     <div class="forms">
-      <vue-form-terminator
-        v-bind="formSetup"
-        :model="model"
-        @submited="handleSubmit"
-      ></vue-form-terminator>
-      <vue-form-terminator
-        v-bind="formSetup2"
-        :model="model2"
-        @submited="handleSubmit"
-      ></vue-form-terminator>
+      <vue-form-terminator v-bind="formSetup" :model="model" @submited="handleSubmit"></vue-form-terminator>
+      <vue-form-terminator v-bind="formSetup2" :model="model2" @submited="handleSubmit"></vue-form-terminator>
     </div>
   </div>
 </template>
@@ -53,7 +40,7 @@ export default {
         username: "Mirkasin",
         email: "mirko@gmail.com",
         password: "12345",
-        repeatPassword: "12345",
+        repeatPassword: "12345"
       },
 
       formSetup: {
@@ -69,14 +56,14 @@ export default {
               placeholder: "First name",
               validations: {
                 required: {
-                  message: "First name is required",
+                  message: "First name is required"
                 },
                 max: {
                   value: 30,
-                  message: "First name must have less then 20 characters",
-                },
+                  message: "First name must have less then 20 characters"
+                }
               },
-              otherClasses: "ui input custom-input-semanticui",
+              otherClasses: "ui input custom-input-semanticui"
             },
             {
               id: "lastName",
@@ -86,15 +73,15 @@ export default {
               placeholder: "Last name",
               validations: {
                 required: {
-                  message: "Last name is required",
+                  message: "Last name is required"
                 },
                 max: {
                   value: 30,
-                  message: "Last name must have less then 20 characters",
-                },
+                  message: "Last name must have less then 20 characters"
+                }
               },
-              otherClasses: "ui input custom-input-semanticui",
-            },
+              otherClasses: "ui input custom-input-semanticui"
+            }
           ],
           {
             id: "username",
@@ -104,14 +91,14 @@ export default {
             placeholder: "Username",
             validations: {
               required: {
-                message: "Username is required",
+                message: "Username is required"
               },
               max: {
                 value: 20,
-                message: "Username must have less then 20 characters",
-              },
+                message: "Username must have less then 20 characters"
+              }
             },
-            otherClasses: "ui input custom-input-semanticui",
+            otherClasses: "ui input custom-input-semanticui"
           },
           {
             id: "email",
@@ -121,13 +108,13 @@ export default {
             placeholder: "Email",
             validations: {
               required: {
-                message: "Email is required",
+                message: "Email is required"
               },
               email: {
-                message: "Email must be a valid email",
-              },
+                message: "Email must be a valid email"
+              }
             },
-            otherClasses: "ui input custom-input-semanticui",
+            otherClasses: "ui input custom-input-semanticui"
           },
           {
             id: "password",
@@ -137,18 +124,18 @@ export default {
             placeholder: "Password",
             validations: {
               required: {
-                message: "Password is required",
+                message: "Password is required"
               },
               min: {
                 value: 5,
-                message: "Pasword must have more then 5 characters",
+                message: "Pasword must have more then 5 characters"
               },
               max: {
                 value: 20,
-                message: "Pasword must have less then 20 characters",
-              },
+                message: "Pasword must have less then 20 characters"
+              }
             },
-            otherClasses: "ui input custom-input-semanticui",
+            otherClasses: "ui input custom-input-semanticui"
           },
           {
             id: "repeatPassword",
@@ -158,23 +145,23 @@ export default {
             placeholder: "Repeat Password",
             validations: {
               required: {
-                message: "Repeat Pasword is required",
+                message: "Repeat Pasword is required"
               },
               min: {
                 value: 5,
-                message: "Must have more then 5 characters",
+                message: "Must have more then 5 characters"
               },
               max: {
                 value: 20,
-                message: "Repeat Pasword must have less then 20 characters",
+                message: "Repeat Pasword must have less then 20 characters"
               },
               compareElements: {
                 value: "password",
-                message: "Repeat Password must be equal as password",
-              },
+                message: "Repeat Password must be equal as password"
+              }
             },
-            otherClasses: "ui input custom-input-semanticui",
-          },
+            otherClasses: "ui input custom-input-semanticui"
+          }
         ],
         actions: [
           {
@@ -182,20 +169,20 @@ export default {
             name: "Submit",
             type: "submit",
             icon: "fas fa-check-circle",
-            otherClasses: "ui primary button custom-button",
+            otherClasses: "ui primary button custom-button"
           },
           {
             id: "reset",
             name: "Reset",
             type: "Reset",
             icon: "fas fa-times-circle",
-            otherClasses: "ui yellow basic button custom-button",
-          },
-        ],
+            otherClasses: "ui yellow basic button custom-button"
+          }
+        ]
       },
 
       model2: {
-        test: "Some test input",
+        test: "Some test input"
       },
 
       formSetup2: {
@@ -204,15 +191,16 @@ export default {
         body: [
           {
             id: "test",
+            type: "text",
             name: "Test",
             placeholder: "test",
             validations: {
               required: {
-                message: "Test is required",
-              },
+                message: "Test is required"
+              }
             },
-            otherClasses: "ui input custom-input-semanticui",
-          },
+            otherClasses: "ui input custom-input-semanticui"
+          }
         ],
         actions: [
           {
@@ -220,25 +208,25 @@ export default {
             name: "Submit",
             type: "submit",
             icon: "fas fa-check-circle",
-            otherClasses: "ui primary button custom-button",
+            otherClasses: "ui primary button custom-button"
           },
           {
             id: "reset2",
             name: "Reset",
             type: "reset",
             icon: "fas fa-times-circle",
-            otherClasses: "ui yellow basic button custom-button",
-          },
-        ],
-      },
+            otherClasses: "ui yellow basic button custom-button"
+          }
+        ]
+      }
     };
   },
 
   methods: {
     handleSubmit(data) {
       console.log(data);
-    },
-  },
+    }
+  }
 };
 </script>
 
